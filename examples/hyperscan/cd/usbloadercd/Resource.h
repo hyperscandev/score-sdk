66f9d8b99ce54db2b8bbb88287dc0860836ee361
ppcasm <ppcasm@gmail.com>
2025-05-16 03:21:29 -0400

    Restructure SDK

---
+++ examples/hyperscan/cd/usbloadercd/Resource.h
@@ -0,0 +1,11 @@
+// SUNPLUS  S+core IDE generated include file.
+#ifndef HYPERSCAN_RESOURCE_H__15724_19169__INCLUDED_
+#define HYPERSCAN_RESOURCE_H__15724_19169__INCLUDED_
+
+extern unsigned char* _binary_C__Users_ppcasm_Documents_s_core_sdk_examples_hyperscan_cd_usbloadercd_cdusbldr_bin_start;
+#define RES_CDUSBLDR_BIN (unsigned char*)&_binary_C__Users_ppcasm_Documents_s_core_sdk_examples_hyperscan_cd_usbloadercd_cdusbldr_bin_start
+extern unsigned char* _binary_C__Users_ppcasm_Documents_s_core_sdk_examples_hyperscan_cd_usbloadercd_cdusbldr_bin_end;
+#define RES_CDUSBLDR_BIN_END (unsigned char*)&_binary_C__Users_ppcasm_Documents_s_core_sdk_examples_hyperscan_cd_usbloadercd_cdusbldr_bin_end
+
+
+#endif //HYPERSCAN_RESOURCE_H__15724_19169__INCLUDED_
